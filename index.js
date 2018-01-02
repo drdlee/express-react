8fc2b2036ab31347b68a13b785e42691e6813d60
drdlee <danielrudi.lee@gmail.com>
2018-01-02 11:35:49 +0800

    passportJS : using passportJS and GoogleStrategy

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const app = require('express')(),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	passport = require('passport'),
+	GoogleStrategy = require('passport-google-oauth20').Strategy,
+	keys = require('./config/keys'); // this key is really important, so it will not pushed to git
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there', let: 'me try again' });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback' // user will redirect to this is the url after grant us access to their google+ information
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+); // authenticate using GoogleStrategy by calling 'google',
+// because inside GoogleStrategy already declared the string 'google'
+// a scope is the information that google gives us to access about the user,
+// there is quite some scope, can just google it
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-// process.env.PORT is for heroku
-// to assign the port later on after we deploy the app.
-// 5000 is for localhost on my PC
