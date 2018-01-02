49a2b549f68cc498a1ce8713dce18b6c02a728b9
drdlee <danielrudi.lee@gmail.com>
2018-01-02 12:15:57 +0800

    refactoring the files

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-const app = require('express')(),
-	bodyParser = require('body-parser'),
-	passport = require('passport'),
-	GoogleStrategy = require('passport-google-oauth20').Strategy,
-	keys = require('./config/keys');
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile', profile);
-		} // so here is the action / data we get after user grant us access to their information
-	)
-);
-
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['profile', 'email']
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-// here is the redirected route after user grant us access to their information
+const app = require('express')();
+require('./services/passport');
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
