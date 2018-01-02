9209f8fb2336dbe58907a3d21fb8d22e49d070eb
drdlee <danielrudi.lee@gmail.com>
2018-01-02 11:58:00 +0800

    passportJS : getting data back from google

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const app = require('express')(),
 	bodyParser = require('body-parser'),
 	passport = require('passport'),
 	GoogleStrategy = require('passport-google-oauth20').Strategy,
-	keys = require('./config/keys'); // this key is really important, so it will not pushed to git
+	keys = require('./config/keys');
 
 passport.use(
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback' // user will redirect to this is the url after grant us access to their google+ information
+			callbackURL: '/auth/google/callback'
 		},
-		accessToken => {
-			console.log(accessToken);
-		}
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken);
+			console.log('profile', profile);
+		} // so here is the action / data we get after user grant us access to their information
 	)
 );
 
@@ -22,10 +24,10 @@ app.get(
 	passport.authenticate('google', {
 		scope: ['profile', 'email']
 	})
-); // authenticate using GoogleStrategy by calling 'google',
-// because inside GoogleStrategy already declared the string 'google'
-// a scope is the information that google gives us to access about the user,
-// there is quite some scope, can just google it
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+// here is the redirected route after user grant us access to their information
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
