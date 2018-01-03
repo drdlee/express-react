b722f58648f65669b5561cf67facb7a45aab9df7
drdlee <danielrudi.lee@gmail.com>
2018-01-03 22:40:56 +0800

    Express React: communicating express to react

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(passport.session());
 
 require('./routes/authRoute')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
 	console.log('connected :5000');
